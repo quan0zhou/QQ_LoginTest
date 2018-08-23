024994af3c8133bc703f4be26c692263e550c8c3
周全 <quan0zhou@163.com>
2018-08-23 17:25:02 +0800

    获取原图方式发生改变

--- QQ_LoginTest/Program.cs
+++ QQ_LoginTest/Program.cs
@@ -8,7 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-
+using System.Text.RegularExpressions;
 
 namespace QQ_LoginTest
 {
@@ -51,10 +51,11 @@ namespace QQ_LoginTest
             Console.WriteLine(slideBkg.Size.Width);
             Console.WriteLine(slideBkg.Size.Height);
 
-            //例如（带阴影）地址：https://ssl.captcha.qq.com/cap_union_new_getcapbysig?aid=549000912&asig=&captype=&protocol=https&clientype=2&disturblevel=&apptype=2&curenv=inner&ua=TW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV09XNjQpIEFwcGxlV2ViS2l0LzUzNy4zNiAoS0hUTUwsIGxpa2UgR2Vja28pIENocm9tZS82NC4wLjMyODIuMjUgU2FmYXJpLzUzNy4zNg==&sess=GLJWtGBjtiY9IU65Duozl2CTJgVIeeI5j0T-aRhSyUHy1n_Hw4tI5hTPv8L4cVel2MHC-Tcr60K7Bn8i61wc7QIGZtkTtRHvxSNrCZPKeIix_yPRM6DVRgPABbeCtxomRu2iAIPw4catxqVGExbTMPutJWm4_prJkGe4jpALYW6-EO24eqD8PMCUTDSycH72eAEDAhmSKK8*&theme=&noBorder=noborder&fb=1&showtype=embed&uid=222222222&cap_cd=ZJBJeRGBPVMXgLfm8Q6NEMfhQUuBD25K8Z1dButH6TX_U0f-DWE9CQ**&lang=2052&rnd=828269&rand=0.22818125792486477&vsig=c01HtB-jbxc5jTAG-7hEWHT2NEe0uXQbqceH5xovPH4TrfUU7sofwExBehTib403f-aSAWGThq9_HPwhXZxO9m-d7EqoOMdVTaCkje30e5bjEqUM5wVvIr8VEGFKFIvu7HOxTJaz4R0cb9Hnb-7Sigh7INiSdr7uWSdjcpmUJgxrdAxsFz2xPSPLA**&img_index=1
+            //例如（带阴影）地址：https://hy.captcha.qq.com/hycdn_1_1585770526512857856_0
             string newUrl = slideBkg.GetAttribute("src");
-            //原图地址：https://ssl.captcha.qq.com/cap_union_new_getcapbysig?aid=549000912&asig=&captype=&protocol=https&clientype=2&disturblevel=&apptype=2&curenv=inner&ua=TW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV09XNjQpIEFwcGxlV2ViS2l0LzUzNy4zNiAoS0hUTUwsIGxpa2UgR2Vja28pIENocm9tZS82NC4wLjMyODIuMjUgU2FmYXJpLzUzNy4zNg==&sess=GLJWtGBjtiY9IU65Duozl2CTJgVIeeI5j0T-aRhSyUHy1n_Hw4tI5hTPv8L4cVel2MHC-Tcr60K7Bn8i61wc7QIGZtkTtRHvxSNrCZPKeIix_yPRM6DVRgPABbeCtxomRu2iAIPw4catxqVGExbTMPutJWm4_prJkGe4jpALYW6-EO24eqD8PMCUTDSycH72eAEDAhmSKK8*&theme=&noBorder=noborder&fb=1&showtype=embed&uid=222222222&cap_cd=ZJBJeRGBPVMXgLfm8Q6NEMfhQUuBD25K8Z1dButH6TX_U0f-DWE9CQ**&lang=2052&rnd=828269&rand=0.22818125792486477&vsig=c01HtB-jbxc5jTAG-7hEWHT2NEe0uXQbqceH5xovPH4TrfUU7sofwExBehTib403f-aSAWGThq9_HPwhXZxO9m-d7EqoOMdVTaCkje30e5bjEqUM5wVvIr8VEGFKFIvu7HOxTJaz4R0cb9Hnb-7Sigh7INiSdr7uWSdjcpmUJgxrdAxsFz2xPSPLA**&img_index=0
-            string oldUrl = newUrl.Substring(0, newUrl.Length - 1) + "0";
+            //原图地址：https://hy.captcha.qq.com/hycdn_0_1585770526512857856_0
+            //用正则匹配并替换
+            string oldUrl = new Regex(@"_\d_", RegexOptions.Multiline).Replace(newUrl,"_0_");
             //根据地址获取到图像
             Bitmap oldBmp = (Bitmap)LoginHelper.GetImg(oldUrl);
             Bitmap newBmp = (Bitmap)LoginHelper.GetImg(newUrl);
